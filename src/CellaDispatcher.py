05e45bc35fe6641cc4d9542bc62a45566192091c
sebastien-choblet-klocel <79571573+sebastien-choblet-klocel@users.noreply.github.com>
2023-10-09 22:38:08 +0200

    dddd

--- src/CellaDispatcher.py
+++ src/CellaDispatcher.py
@@ -110,7 +110,7 @@ def init_logs(config: configparser.ConfigParser) -> None:
     )
     logging.info("Start KloDispatcher")
 
-    testdir = path.abspath(path.join(path.dirname(__file__), 'SumatraPDF.exe'))
+    testdir = path.abspath(path.join(path.dirname(__file__), 'CellaDispatcher.exe'))
     logging.info("SCH IXI 2nd commit.,,,.")
     logging.info(testdir)
 
